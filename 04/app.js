eb066e61bfcaf1675fa4937d738561c206c3e4a7
Piotr Mazurek <mazurekp93@gmail.com>
2023-05-30 23:07:01 +0200

    Finish task-04

--- 04/app.js
+++ 04/app.js
@@ -3,6 +3,23 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
+
+    const colorElement = document.querySelector('input[type=color]')
+    const rangeElement = document.querySelector('input[type=range]')
+
+    colorElement.addEventListener('change', function () {
+        const color = colorElement.value
+        setBoxShadow(boxElement, color, rangeElement.value)
+    })
+    rangeElement.addEventListener('change', handleRange)
+    rangeElement.addEventListener('mousemove', handleRange)
+
+
+    function handleRange() {
+        const opacity = rangeElement.value / 100
+        setBoxShadow(boxElement, colorElement.value, opacity)
+    }
+
 }
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
@@ -19,7 +36,7 @@ function setBoxShadow(element, colorInHex, opacity = 1) {
 
 function getChannelColor(colorInHex, channelName) {
     let start;
-    switch(channelName) {
+    switch (channelName) {
         case 'red':
             start = 1;
             break;
@@ -34,7 +51,9 @@ function getChannelColor(colorInHex, channelName) {
     const channelColorHex = colorInHex.substr(start, 2);
     const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+    return channelColorDec;
 }
+
+
 
 
